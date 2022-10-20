c28e541962b6314e09dc4af22d45ff4c61f290d0
Julio Cesar Gabardo <julio.gabardo@alunos.sc.senac.br>
2022-10-19 21:48:00 -0300

    ultimo da aula 19/10

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,17 @@
 	 document.getElementById("data").innerHTML=Date();
  }
  
+ var nome=prompt("Digite o seu nome: ");
+ alert(nome)
+ var idade=prompt("Digite a sua idade: ");
+ var acordado="true";
+ var vetor=[1,"lucas",true]
+ alert(vetor)
  
+ document.write("Meu nome é ",nome," e tenho ",idade, "anos.");
+ alert("Meu nome é: "+nome+" minha idade é: "+idade);
+ 
+ var n1 = prompt("Digite o primeiro numero: ");
+ var n2 = prompt("Digite o segundo numero: ");
+ 
+ document.write("A soma é= ",parseInt(n1)+parseInt(n2));
