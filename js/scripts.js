10d57614e7c011ad4741db021db1557ed01864f7
Julio Cesar Gabardo <julio.gabardo@alunos.sc.senac.br>
2022-10-19 21:31:20 -0300

    Segundo update html e js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+ alert('Este é um alerta')<!-- alerta basico,janela na pagina
+ 
+ document.write("Hello Moto");<!-- Escreve no corpo da pagina
+ document.write("<h2>"+document.title+"</h2>");<!-- Alterando propriedade via javaScript.
+ 
+ function mostraData(){
+	 document.getElementById("data").innerHTML=Date();
+ }
+ 
+ 
